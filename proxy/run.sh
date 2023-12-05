2f4f6a96b3eea45ed0fe098f2d0099e4d3104843
moprules <rav-navini-gego-cutropal@yandex.ru>
2023-12-05 23:03:04 +0300

    return $

--- proxy/run.sh
+++ proxy/run.sh
@@ -2,17 +2,16 @@
 
 # 1. Получаем/обновляем/используем старые сертификаты от certbot
 # Если сертификатов еще не было (папки с сертификатами не существует)
-if [[ ! -d /etc/letsencrypt/live/${domain}/ ]]; then
+if [[ ! -d /etc/letsencrypt/live/${DOMAIN}/ ]]; then
     # то получаем их впервые
-    certbot certonly --standalone --webroot --webroot-path=/certbot --register-unsafely-without-email --agree-tos -d $domain
+    certbot certonly --standalone --webroot --webroot-path=/certbot --register-unsafely-without-email --agree-tos -d ${DOMAIN}
 fi
-    
-# 2. на всякий случай пытаемся обновить существующий сертификат
-# Если еще рано его обновлять, то ничего делать не будет
-certbot renew
 
-# 3. Запускаем cron для ежедневной проверки сертификатов
+# 2. Запускаем cron для ежедневной проверки сертификатов
 crond -b -l 8 -L /app/cron.log
+
+# 3. Задаем конфиг nginx для использования ssl
+envsubst < ./nginx.conf > /etc/nginx/conf.d/default.conf
 
 # 4. Запуск nginx в интерактивном режиме
 # Теперь nginx главные процесс, если с ним что-то случится, то контейнер перезапустится
