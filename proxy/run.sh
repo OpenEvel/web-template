75f00238381be56adbf6d1358c995573e1e75a36
moprules <rav-navini-gego-cutropal@yandex.ru>
2023-12-05 22:40:01 +0300

    new structure

---
+++ proxy/run.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# 1. Получаем/обновляем/используем старые сертификаты от certbot
+# Если сертификатов еще не было (папки с сертификатами не существует)
+if [[ ! -d /etc/letsencrypt/live/${domain}/ ]]; then
+    # то получаем их впервые
+    certbot certonly --standalone --webroot --webroot-path=/certbot --register-unsafely-without-email --agree-tos -d $domain
+fi
+    
+# 2. на всякий случай пытаемся обновить существующий сертификат
+# Если еще рано его обновлять, то ничего делать не будет
+certbot renew
+
+# 3. Запускаем cron для ежедневной проверки сертификатов
+crond -b -l 8 -L /app/cron.log
+
+# 4. Запуск nginx в интерактивном режиме
+# Теперь nginx главные процесс, если с ним что-то случится, то контейнер перезапустится
+nginx -g 'daemon off;'"
